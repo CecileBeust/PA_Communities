6b8083267b431e3a4826437118285a3d13f4b7f8
Cecile Beust <cecile.beust@gmail.com>
2023-06-19 10:47:11 +0200

    edit tests_CI.py

--- Tests/tests_CI.py
+++ Tests/tests_CI.py
@@ -1,22 +1,41 @@
-from create_config_seed_files import build_seeds_file
 import os
+import pandas as pd
+import numpy as np
+from itRWR import create_config_seed_files
 
-dico_seeds = build_seeds_file("orpha_codes_PA.txt")
-path = '/home/cbeust/Landscape_PA/CommunityIdentification/CommunityIdentification_V2'
+path = os.path.dirname(os.path.realpath(__file__))
+path = path + '/'
+os.chdir(path)
+print(path)
 
-def test_no_seeds(dico_seeds: dict, path: str, list_sizes: list):
-    list_id_not_analyzed = []
-    for disease in dico_seeds.keys():
-        if dico_seeds[disease] == []:
-            list_id_not_analyzed.append(disease)
-    for size in list_sizes:
-        for disease_not_analyzed in list_id_not_analyzed:
-            # check this disease has not been analyzed = check result path does not exist
-            assert not os.path.exists(f"{path}/results_{size}_{disease_not_analyzed}/seeds_{disease_not_analyzed}.txt")
+data_folder = os.path.join(os.path.dirname(__file__), '..', 'data')
+data_file_path = os.path.join(data_folder, 'orpha_codes_PA.txt')
+
+dico_seeds = create_config_seed_files.build_seeds_file(data_file_path)
+
+def test_no_seeds_diseases(path: str, size: int) -> list:
+    """Function to test if the diseases which are not associated to any
+    seed are not analyzed during the identification of communities
+
+    Args:
+        path (str): path to current Tests folder
+        sizes (int): number of iterations used for itRWR
+    
+    Returns:
+        list: a list of diseases not analyzed because not assiciated to
+        any seed
+    """
+    df = pd.read_csv(data_file_path)
+    list_id_not_analyzed = list()
+    for index, row in df.iterrows():
+        if row[2] is np.nan or not pd.isnull(row[2]):
+            list_id_not_analyzed.append(row[1])
+    for disease_not_analyzed in list_id_not_analyzed:
+        # check this disease has not been analyzed = check result path does not exist
+        assert not os.path.exists(f"{path}/../IDCommunity/results_{size}_{disease_not_analyzed}/seeds_{disease_not_analyzed}.txt")
     return list_id_not_analyzed
 
-#list_id_not_analyzed = test_no_seeds(dico_seeds, path, [100])
-list_id_not_analyzed = test_no_seeds(dico_seeds, path, [100])
+#list_id_not_analyzed = test_no_seeds_diseases(path, 10)
 
 def test_communities(dico_seeds: dict, not_analyzed: list, path, size: int):
     disease_analyzed = [disease for disease in dico_seeds.keys() if disease not in not_analyzed]
@@ -28,7 +47,7 @@ def test_communities(dico_seeds: dict, not_analyzed: list, path, size: int):
             for line in file:
                 nb_seeds += 1
                 seeds_disease.append(line.rstrip())
-        community_disease_file = f"{path}/results_{size}_{disease}/seeds_{disease}.txt"
+        community_disease_file = f"{path}/../IDCommunity/results_{size}_{disease}/seeds_{disease}.txt"
         community_size = 0
         community_disease_nodes = []
         with open(community_disease_file, 'r') as file:
@@ -43,13 +62,12 @@ def test_communities(dico_seeds: dict, not_analyzed: list, path, size: int):
         
         
 #test_communities(dico_seeds, list_id_not_analyzed, path, 100)
-test_communities(dico_seeds, list_id_not_analyzed, path, 50)
 
 def test_config_files(dico_seeds: dict, not_analyzed: list, path, size):
     disease_analyzed = [disease for disease in dico_seeds.keys() if disease not in not_analyzed]
     for disease in disease_analyzed:
-        config_disease = f"{path}/config_{disease}.yml"
-        config_community = f"{path}/results_{size}_{disease}/seeds_{disease}.txt"
+        config_disease = f"{path}/../IDCommunity/config_{disease}.yml"
+        config_community = f"{path}../IDCommunity/results_{size}_{disease}/seeds_{disease}.txt"
         with open(config_disease, 'r') as file:
             contents = file.read()
             comp = "seed: seeds_{disease}.txt \
@@ -59,14 +77,13 @@ def test_config_files(dico_seeds: dict, not_analyzed: list, path, size):
                     multiplex: \
                         1: \
                             layers: \
-                                - multiplex/1/PPI_HiUnion_LitBM_APID_gene_names_190123.tsv \
-                                - multiplex/1/reactome_pathways_gene_names_190123.tsv \
-                                - multiplex/1/Coexpression_310323.tsv \
-                                - multiplex/1/Complexes_gene_names_190123.tsv \
-                                - multiplex/1/Gene_involved_in_diseases_layer_020223.tsv \
+                                - multiplex/1/Coexpression.tsv \
+                                - multiplex/1/Complexes.tsv \
+                                - multiplex/1/PPI.tsv \
+                                - multiplex/1/Pathways.tsv \
                             delta: 0.5 \
-                            graph_type: [00, 00, 00, 00, 00] \
-                            tau: [0.2, 0.2, 0.2, 0.2, 0.2]"
+                            graph_type: [00, 00, 00, 00] \
+                            tau: [0.25, 0.25, 0.25, 0.25]"
             assert contents == comp
         with open(config_community, 'r') as file:
             contents = file.read()
@@ -77,14 +94,13 @@ def test_config_files(dico_seeds: dict, not_analyzed: list, path, size):
                     multiplex: \
                         1: \
                             layers: \
-                                - multiplex/1/PPI_HiUnion_LitBM_APID_gene_names_190123.tsv \
-                                - multiplex/1/reactome_pathways_gene_names_190123.tsv \
-                                - multiplex/1/Coexpression_310323.tsv \
-                                - multiplex/1/Complexes_gene_names_190123.tsv \
-                                - multiplex/1/Gene_involved_in_diseases_layer_020223.tsv \
+                                - multiplex/1/Coexpression.tsv \
+                                - multiplex/1/Complexes.tsv \
+                                - multiplex/1/PPI.tsv \
+                                - multiplex/1/Pathways.tsv \
                             delta: 0.5 \
-                            graph_type: [00, 00, 00, 00, 00] \
-                            tau: [0.2, 0.2, 0.2, 0.2, 0.2]"
+                            graph_type: [00, 00, 00, 00] \
+                            tau: [0.25, 0.25, 0.25, 0.25]"
             assert contents == comp
 
-#test_config_files(dico_seeds, list_id_not_analyzed, path, 100)
+#test_config_files(dico_seeds, list_id_not_analyzed, path, 10)
