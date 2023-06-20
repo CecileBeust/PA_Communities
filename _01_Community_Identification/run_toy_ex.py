f0815f15b5251efc55bb053c77963ea0f28d81af
Cecile Beust <cecile.beust@gmail.com>
2023-06-20 14:14:59 +0200

    update repo

--- _01_Community_Identification/run_toy_ex.py
+++ _01_Community_Identification/run_toy_ex.py
@@ -5,6 +5,6 @@ path = os.path.dirname(os.path.realpath(__file__))
 path = path + '/'
 os.chdir(path)
 
-diseases = path + "orpha_codes_toy_ex.txt"
-num_iteration = 10
+diseases = path + "orpha_codes_PA.txt"
+num_iteration = 100
 community_identification(path, diseases, num_iteration)
